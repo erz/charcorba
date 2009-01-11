fa8d3ff0f62c46997be8967deb2d379c4a5e8919
alaeffar <alaeffar@21f5bc8e-9605-11dd-8626-7fb1ecac6e6f>
2009-01-11 01:15:41 +0000

    tags

--- trunk/charcorba-java/by_mr19cm/JMenuBarreMenu.java
+++ trunk/charcorba-java/by_mr19cm/JMenuBarreMenu.java
@@ -3,6 +3,7 @@ package by_mr19cm;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JCheckBox;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -24,12 +25,24 @@ public class JMenuBarreMenu extends JMenuBar {
 				JMenuBarreMenu.this.client.getContacts();
 			}
 		});
-		JMenuItem itemDeconnexion = new JMenuItem("Dconnexion");
+		JMenuItem itemDeconnexion = new JMenuItem("Deconnexion");
 		
 		menuConnexion.add(itemConnexion);
 		menuConnexion.add(itemDeconnexion);
 		
 		this.add(menuConnexion);
+		
+		//Affichage des contacts selon leur tags
+		JMenu menuTags = new JMenu("Tags");
+		for(int i = 0; i < JMenuBarreMenu.this.client.getListeContacts().length;++i)
+		{
+			for (int j = 0; j < JMenuBarreMenu.this.client.getListeContacts()[i].getTags().size(); ++j)
+			{
+				menuTags.add(new JCheckBox(JMenuBarreMenu.this.client.getListeContacts()[i].getTags().get(j)));
+			}
+		}
+		this.add(menuTags);
+		
 	}
 
 }
